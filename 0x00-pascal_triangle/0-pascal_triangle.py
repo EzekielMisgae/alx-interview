048e499ee882f98f30602f5568c248df6083a9ec
ezekielalene <ezkielmisgae@gmail.com>
2023-01-26 02:28:09 +0300

    Make it executable

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,23 +2,12 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
+
     triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
+    for i in range(n - 1):
+        temp = [0] + triangle[-1] + [0]
+        row = []
+        for j in range(len(triangle[-1]) + 1):
+            row.append(temp[j] + temp[j + 1]
         triangle.append(row)
     return triangle
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
-if __name__ == "__main__":
-    triangle = pascal_triangle(5)
-    print_triangle(triangle)
