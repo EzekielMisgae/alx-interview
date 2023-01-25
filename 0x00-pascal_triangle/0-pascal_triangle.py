358823e12455271ac9f6bb8ca19d0f5fa0e1cae4
ezekielalene <ezkielmisgae@gmail.com>
2023-01-26 02:13:00 +0300

    Print pascal triangle on the screen

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,3 +10,15 @@ def pascal_triangle(n):
         row.append(1)
         triangle.append(row)
     return triangle
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    triangle = pascal_triangle(5)
+    print_triangle(triangle)
