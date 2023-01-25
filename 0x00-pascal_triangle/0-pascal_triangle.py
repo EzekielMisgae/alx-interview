b9e2b181222dd8f0c4b5585ded477d6a1fe45f3c
ezekielalene <ezkielmisgae@gmail.com>
2023-01-26 02:30:31 +0300

    Make it executable

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+""" Gets the function of the Pascal's triangle"""
+
+
 def pascal_triangle(n):
+    """That returns a list of lists of integers
+representing the Pascal's triangle """
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    res = [[1]]
     for i in range(n - 1):
-        temp = [0] + triangle[-1] + [0]
+        temp = [0] + res[-1] + [0]
         row = []
-        for j in range(len(triangle[-1]) + 1):
-            row.append(temp[j] + temp[j + 1]
-        triangle.append(row)
-    return triangle
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+    return res
