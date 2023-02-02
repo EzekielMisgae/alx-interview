28f38a51ecb1c618649b25c89f39e23e3525a488
ezekielalene <ezkielmisgae@gmail.com>
2023-02-03 01:12:18 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-
+"""Determines if all boxes can be opened """
 
 
 def canUnlockAll(boxes):
-    """ method that determines if all the boxes can be opened. """
-    n = len(boxes)
-    myList = [0]
-    for i in myList:
+    """ Determines if all the boxes can be opened """
+    # Set of visited boxes
+    visited = set()
+
+    # Recursive helper function to explore all reachable boxes
+    def dfs(i):
+        # Mark the current box as visited
+        visited.add(i)
+
+        # Explore each box that can be reached from the current box
         for j in boxes[i]:
-            if j not in myList:
-                if j < n:
-                    myList.append(j)
-    if len(myList) == n:
-        return True
-    return False
+            if j not in visited:
+                dfs(j)
+
+    # Start the depth-first search from the first box (which is unlocked)
+    dfs(0)
+
+    return len(visited) == len(boxes)
