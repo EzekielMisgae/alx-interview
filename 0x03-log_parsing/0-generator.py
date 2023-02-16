818c8f393d3b10303a7cf21bdfe45ea2c8777651
ezekielalene <ezkielmisgae@gmail.com>
2023-02-16 22:58:35 +0300

    Log parsing

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import random
+import datetime
 import sys
 from time import sleep
-import datetime
 
 for i in range(10000):
     sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
+    ip_address = f"{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}"
+    date = datetime.datetime.now().strftime("%d/%b/%Y:%H:%M:%S %z")
+    status_code = random.choice([200, 301, 400, 401, 403, 404, 405, 500])
+    file_size = random.randint(1, 1024)
+    log_line = f"{ip_address} - [{date}] \"GET /projects/260 HTTP/1.1\" {status_code} {file_size}\n"
+    sys.stdout.write(log_line)
     sys.stdout.flush()
