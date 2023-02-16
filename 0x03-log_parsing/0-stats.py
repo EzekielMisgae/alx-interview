7373240227062da82baf593098425d56c587033a
ezekielalene <ezkielmisgae@gmail.com>
2023-02-16 22:56:19 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,45 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+"""Script that reads stdin line by line and computes metrics"""
 
 import sys
 
-# Define the possible status codes
-STATUS_CODES = [200, 301, 400, 401, 403, 404, 405, 500]
 
-# Initialize the metrics
-total_size = 0
-status_code_counts = {code: 0 for code in STATUS_CODES}
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-# Read input line by line and compute metrics
-line_count = 0
-for line in sys.stdin:
-    # Parse the input line
-    try:
-        ip, date, request, status_code, file_size = line.split(' ')
-        status_code = int(status_code)
-        file_size = int(file_size)
-        # We only count lines with the correct format
-        if request != 'GET /projects/260 HTTP/1.1':
-            continue
-    except ValueError:
-        # Skip lines with incorrect format
-        continue
-    
-    # Update the metrics
-    total_size += file_size
-    if status_code in status_code_counts:
-        status_code_counts[status_code] += 1
-    
-    line_count += 1
-    
-    # Print the metrics every 10 lines
-    if line_count % 10 == 0:
-        print(f'Total file size: {total_size}')
-        for code in sorted(status_code_counts):
-            if status_code_counts[code] > 0:
-                print(f'{code}: {status_code_counts[code]}')
-        print()
-        
-# Print the final metrics
-print(f'Total file size: {total_size}')
-for code in sorted(status_code_counts):
-    if status_code_counts[code] > 0:
-        print(f'{code}: {status_code_counts[code]}')
+
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
+
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
+
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
+
+
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
