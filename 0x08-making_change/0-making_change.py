8856891b92ffd846066df46ee953c774cdddc203
ezekielalene <ezkielmisgae@gmail.com>
2023-03-24 00:10:40 +0300

    Make it executable and make it short

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,12 +3,7 @@
 """ Contains makeChange function"""
 
 def makeChange(coins, total):
-    """
-    Returns: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any number of coins you have, return -1
-    """
-    if not coins or coins is None:
+    if not coins:
         return -1
     if total <= 0:
         return 0
@@ -18,7 +13,6 @@ def makeChange(coins, total):
         while coin <= total:
             total -= coin
             change += 1
-        if (total == 0):
+        if total == 0:
             return change
-
     return -1
