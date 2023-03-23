f67e8f98930da1c0348d1d1d7a5060031cc66a39
ezekielalene <ezkielmisgae@gmail.com>
2023-03-24 00:07:17 +0300

    Change comments and function

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-'''Making change challenge'''
 
+""" Contains makeChange function"""
 
 def makeChange(coins, total):
-    # Initialize a list to store the minimum number of coins needed for each total from 0 to the given total
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
     if not coins or coins is None:
         return -1
-    # 0 coins are needed to make a total of 0
     if total <= 0:
         return 0
     change = 0
     coins = sorted(coins)[::-1]
-    # Initialize a list to store the minimum number of coins needed for each total from 0 to the given total
     for coin in coins:
         while coin <= total:
             total -= coin
             change += 1
         if (total == 0):
-            # If the minimum number of coins needed for the given total is still infinity, it means the total cannot be met
             return change
+
     return -1
